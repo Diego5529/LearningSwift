478dd07028d81733babc17bc548bfe687e06e332
Diego Oliveira <diego.6.souza@gmail.com>
2015-12-22 10:29:46 -0200

    initial commit swift

---
+++ MobileB2B/MobileB2B/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MobileB2B
+//
+//  Created by macmini on 12/22/15.
+//  Copyright © 2015 trovata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
