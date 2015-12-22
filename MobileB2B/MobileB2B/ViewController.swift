2d8a239dab29b82527c241690a84453cd47002cd
Diego Oliveira <diego.6.souza@gmail.com>
2015-12-22 18:00:39 -0200

    implementando funcionalidades

--- MobileB2B/MobileB2B/ViewController.swift
+++ MobileB2B/MobileB2B/ViewController.swift
@@ -10,9 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webViewMaster: UIWebView!
+    @IBOutlet var navigationBarMaster: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Abriu o View Controller...")
+        
+        //Set URL
+        setUrlWebView()
+        
+        //Cria a logo
+        let btnName = UIButton()
+        btnName.setImage(UIImage(named: "ImagesPlugtTeste"), forState: .Normal)
+        btnName.frame = CGRectMake(0, 0, 40, 40)
+        btnName.addTarget(self, action: "actionLogoPressed", forControlEvents: .TouchUpInside)
+        
+        //Exibe a Logo
+        let rightBarButton = UIBarButtonItem()
+        rightBarButton.customView = btnName;
+        self.navigationItem.leftBarButtonItem = rightBarButton
+        
+        //Botoes da direita
+        let actionButton = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "actionButtonActionShare")
+        let refreshButton = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: "actionReloadPage")
+        let replyButton = UIBarButtonItem(barButtonSystemItem: .Reply, target: self, action: "actionBackPage")
+        navigationItem.rightBarButtonItems = [replyButton, refreshButton, actionButton]
+    }
+    
+    func setUrlWebView(){
+        let url = NSURL (string: "http://plugt-teste.trovata.com.br")
+        let requestObj = NSURLRequest(URL: url!)
+        webViewMaster.loadRequest(requestObj)
+    }
+    
+    func actionReloadPage()
+    {
+        print("actionReloadPage")
+        webViewMaster.reload();
+    }
+    
+    func actionBackPage()
+    {
+        print("actionBackPage")
+        webViewMaster.goBack();
+    }
+    
+    func actionLogoPressed()
+    {
+        print("Logo Pressed")
+        setUrlWebView()
+    }
+    
+    func actionButtonActionShare()
+    {
+        let currentURL : NSURL = (webViewMaster.request?.URL?.absoluteURL)!
+        print(currentURL)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        navigationItem.title = "Mobile B2B Plugt"
     }
     
     override func didReceiveMemoryWarning() {
